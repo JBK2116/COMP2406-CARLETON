51ac15d6ad1044d20e5e9839046932ad0d505bfb
jovkb <jovani.badinga7@gmail.com>
2025-10-01 23:37:28 -0400

    Created Key Functions -- READ
    Created the functions necessary to dynamically populate
    the category column and menu column.
    
    Bugs Identified:
    1. Selecting default "select a restaurant" doesn't reset header values
    2. Missing Quantity and Price <p> elements in each menu-item

--- AssignmentOne/base-code/js/app.js
+++ AssignmentOne/base-code/js/app.js
@@ -1,6 +1,6 @@
 import {restaurants} from "./client.js"
 
-// ELEMENTS
+// DOM ELEMENTS
 let dropDown = document.getElementById("drop-down");
 
 let pageHeader = document.getElementById("header-h1");
@@ -8,7 +8,10 @@ let restaurantNameHeader = document.getElementById("restaurant-name");
 let minimumOrderHeader = document.getElementById("minimum-order");
 let deliveryFeeHeader = document.getElementById("delivery-fee");
 
+let categoryNamesContainer = document.getElementById("category-names");
+let menuItemsContainer = document.getElementById("menu-container");
 
+// Javascript Variables
 let currentRestaurant;
 
 /**
@@ -26,8 +29,12 @@ function initializeDropDown() {
         dropDown.appendChild(option);
     }
     dropDown.addEventListener("change", () => {
-        currentRestaurant = restaurants.find((r) => r.name === dropDown.value)
+        currentRestaurant = restaurants.find((r) => r.name === dropDown.value);
+        resetCategoryNamesList();
+        resetMenuContainer();
+        
         initializeMainHeaders();
+        initializeCategoryAndMenu();
     })
 }
 
@@ -42,6 +49,111 @@ function initializeMainHeaders() {
     minimumOrderHeader.textContent = `Minimum Order: $${currentRestaurant.min_order.toFixed(2)}`;
     deliveryFeeHeader.textContent = `Delivery Fee: $${currentRestaurant.delivery_charge.toFixed(2)}`;
 }
+
+/**
+ * This function displays the categories and menu items of the current selected restaurant
+ */
+function initializeCategoryAndMenu() {
+    for (let category in currentRestaurant.menu) {
+        let categoryListItem = createCategoryListItem(category);
+        let categoryDiv = createMenuCategoryDiv(category);
+        for (let itemID in currentRestaurant.menu[category]) {
+            let itemObj = currentRestaurant.menu[category][itemID];
+            let itemDiv = createMenuItemDiv(itemObj.name);
+            categoryDiv.appendChild(itemDiv);
+        }
+        categoryNamesContainer.appendChild(categoryListItem);
+        menuItemsContainer.appendChild(categoryDiv);
+    }
+    
+    
+}
+
+/**
+ * Creates a new HTML `<li>` element using the provided category name
+ * 
+ * The returned element is to be used in the category menu column
+ * 
+ * @param {string} categoryName - The name of the category
+ * 
+ * @returns {HTMLLIElement} The created `<li>` element
+ */
+function createCategoryListItem(categoryName) {
+    let listItem = document.createElement("li");
+    let a = document.createElement("a");
+    a.href = `#${categoryName}`
+    a.textContent = categoryName;
+    listItem.appendChild(a);
+    return listItem;
+}
+
+/**
+ * Creates a new `#menu-item` div using the provided arguments
+ * 
+ * @param menuItemName - The name of the menu item
+ * 
+ * @returns The created and filled `#menu-item` div
+ */
+function createMenuItemDiv(menuItemName) {
+    
+    let menuItemDiv = document.createElement("div");
+    menuItemDiv.className = `menu-item`;
+
+    let innerDivHTMLText = document.createElement("p");
+    innerDivHTMLText.className = `item-info`;
+    innerDivHTMLText.textContent = menuItemName;
+
+    let innerDivButton = document.createElement("button");
+    innerDivButton.className = `add-btn`;
+
+    let buttonImage = document.createElement("img");
+    buttonImage.alt = `add button img`;
+    buttonImage.src = "images/add.png";
+    innerDivButton.appendChild(buttonImage);
+
+    menuItemDiv.appendChild(innerDivHTMLText);
+    menuItemDiv.appendChild(innerDivButton);
+    return menuItemDiv;
+
+
+}
+
+/**
+ * Creates and fills a new `#menu-category` div using the provided arguments
+ * 
+ * @param categoryName - The name of the category
+ * 
+ * @returns {HTMLDivElement} The created and filled `#menu-category` div
+ */
+function createMenuCategoryDiv(categoryName) {
+    // Parent Container
+    let rootDiv = document.createElement("div");
+    rootDiv.id = `${categoryName}`;
+    rootDiv.className = `menu-category`;
+    
+    // Container Header
+    let rootDivHeader = document.createElement("h4");
+    rootDivHeader.className = `category-header`;
+    rootDivHeader.textContent = categoryName;
+    
+    rootDiv.appendChild(rootDivHeader);
+    return rootDiv;
+}
+
+/**
+ * This function removes all elements in the `#category-names` list
+ */
+function resetCategoryNamesList() {
+    categoryNamesContainer.innerHTML = "";
+}
+
+/**
+ * This function removes all elements in the `#menu-container` div
+ */
+function resetMenuContainer() {
+    menuItemsContainer.innerHTML = "";
+}
+
 
 
 window.addEventListener("DOMContentLoaded", initializeDropDown)
