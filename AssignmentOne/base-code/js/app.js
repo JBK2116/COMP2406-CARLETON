9e2a47cfca405116a3c46af7f185c36934c17d3b
jovkb <jovani.badinga7@gmail.com>
2025-10-04 13:43:58 -0400

    Add Item functionality complete -- READ
    Add item functionality is complete. This includes
    creating the order-item div, filling it with
    the appropriate information and adding it to the order summary column.
    Functionality to update the order totals is also implemented.
    Remaining functionality is for removing an item from the order summary column.

--- AssignmentOne/base-code/js/app.js
+++ AssignmentOne/base-code/js/app.js
@@ -11,13 +11,20 @@ let deliveryFeeHeader = document.getElementById("delivery-fee");
 let categoryNamesContainer = document.getElementById("category-names");
 let menuItemsContainer = document.getElementById("menu-container");
 
-let orderSummaryContainer = document.getElementById("order-items");
+let orderedItemsContainer = document.getElementById("order-items");
+let subtotalElement = document.getElementById("subtotal-line");
+let deliveryFeeElement = document.getElementById("delivery-fee-line");
+let taxElement = document.getElementById("tax-line");
+let orderTotalElement = document.getElementById("order-total-line");
 
 
 // Javascript Variables
 let currentRestaurant;
 let items = [];
 let orderedItems = [];
+let subtotal = 0;
+let deliveryFee = 0;
+let tax = 0;
 
 class Item {
     constructor(name, description, price) {
@@ -74,6 +81,9 @@ function initializeMainHeaders() {
     restaurantNameHeader.textContent = currentRestaurant.name;
     minimumOrderHeader.textContent = `Minimum Order: $${currentRestaurant.min_order.toFixed(2)}`;
     deliveryFeeHeader.textContent = `Delivery Fee: $${currentRestaurant.delivery_charge.toFixed(2)}`;
+    deliveryFeeElement.textContent = `$${currentRestaurant.delivery_charge.toFixed(2)}`;
+    deliveryFee = currentRestaurant.delivery_charge;
+    updateFinalOrderTotal();
 }
 
 /**
@@ -104,9 +114,15 @@ function initializeOrderSummary() {
     let menuItemContainers = menuItemsContainer.querySelectorAll(".menu-item");
     menuItemContainers.forEach((container) => {
         let addBtn = container.querySelector("img");
-        addBtn.addEventListener("click",() => {
+        addBtn.addEventListener("click", () => {
             let itemObj = items[container.dataset.index];
-            addOrderItemDiv(itemObj, container.dataset.index);
+            let existingOrderItemDiv = getOrderItemDiv(container.dataset.index);
+            if (existingOrderItemDiv) {
+                updateOrderItemDiv(itemObj, existingOrderItemDiv)
+            } else {
+                addOrderItemDiv(itemObj, container.dataset.index);
+            }
+            updateOrderTotals(itemObj, true);
         })
     })
 }
@@ -196,42 +212,58 @@ function createMenuCategoryDiv(categoryName) {
 
 /**
  * This helper function creates and adds an #order-item div to the #order-items container
- *
+ * It additionally configures an event handler for its associated `remove btn`
+ * 
  * @param Item - The Item object to add
  * @param index - The index of the item in the `items` array
  */
-function addOrderItemDiv(item, index) {
+function addOrderItemDiv(Item, index) {
     let orderItemRootContainer = document.createElement("div");
     orderItemRootContainer.dataset.index = index;
     orderItemRootContainer.className = "order-item";
 
     let orderItemDetailsContainer = document.createElement("div");
     orderItemDetailsContainer.className = "order-item-details";
-    
+
     let itemNameContainer = document.createElement("span");
     itemNameContainer.className = "item-name";
-    itemNameContainer.textContent = `${item.name}`;
+    itemNameContainer.textContent = `${Item.name}`;
     let itemQuantityContainer = document.createElement("span");
     itemQuantityContainer.className = "item-quantity";
-    itemQuantityContainer.textContent = `Quantity: ${item.orderedQuantity}`;
+    Item.orderedQuantity += 1;
+    itemQuantityContainer.textContent = `Quantity: ${Item.orderedQuantity}`;
     let itemPriceContainer = document.createElement("span");
     itemPriceContainer.className = "item-total";
-    itemPriceContainer.textContent = `Price: $${item.price.toFixed(2)}`;
-    
+    itemPriceContainer.textContent = `Price: $${Item.price.toFixed(2)}`;
+
     let removeBtn = document.createElement("button");
     removeBtn.className = "btn remove-btn";
-    
+
     let removeBtnImage = document.createElement("img");
     removeBtnImage.alt = "item";
     removeBtnImage.class = "order-item-image";
     removeBtnImage.src = "images/remove.png";
-    
+
     removeBtn.appendChild(removeBtnImage);
-    
+
     orderItemDetailsContainer.append(itemNameContainer, itemQuantityContainer, itemPriceContainer);
     orderItemRootContainer.append(orderItemDetailsContainer, removeBtn);
 
-    orderSummaryContainer.appendChild(orderItemRootContainer);
+    orderedItemsContainer.appendChild(orderItemRootContainer);
+}
+
+/**
+ * This helper function updates the details of an `.order-item` div in the order-summary column
+ *
+ * @param Item - The Item object belong to the div
+ * @param Div - The Div element storing the details
+ */
+function updateOrderItemDiv(Item, Div) {
+    let quantitySpan = Div.querySelector(".item-quantity");
+    let priceSpan = Div.querySelector(".item-total");
+    Item.orderedQuantity += 1;
+    quantitySpan.textContent = `Quantity: ${Item.orderedQuantity}`;
+    priceSpan.textContent = `Price: $${(Item.price * Item.orderedQuantity).toFixed(2)}`;
 }
 
 /**
@@ -239,7 +271,7 @@ function addOrderItemDiv(item, index) {
  *
  * @param Item - The Item object to remove
  */
-function removeOrderItemDiv(item) {
+function removeOrderItemDiv(Item) {
     // TODO: Implement This
 }
 
@@ -248,9 +280,33 @@ function removeOrderItemDiv(item) {
  * function is to be called when adding or removing an Item from the order summary
  *
  * @param Item - The Item object being added or removed
+ * @param add - Boolean value set to true to indicate Item is being added else item is being removed
  */
-function updateOrderTotals(Item) {
-    // TODO: Implement This
+function updateOrderTotals(Item, add) {
+    if (add) {
+        subtotal += Item.price;
+    } else {
+        subtotal -= Item.price;
+    }
+    tax = subtotal * 0.13;
+    subtotalElement.textContent = `$${subtotal.toFixed(2)}`;
+    taxElement.textContent = `$${tax.toFixed(2)}`;
+    updateFinalOrderTotal();
+}
+
+function updateFinalOrderTotal() {
+    let finalOrderTotal = subtotal + tax + deliveryFee
+    orderTotalElement.textContent = `$${finalOrderTotal.toFixed(2)}`;
+}
+
+/**
+ * This function retrieves the `.order-item` div with the matching index data attribute
+ *
+ *@param index - The index of the Item
+ *@returns HTMLDivElement - The matching `order-item` div
+ */
+function getOrderItemDiv(index) {
+    return orderedItemsContainer.querySelector(`#order-items > div[data-index="${index}"]`);
 }
 
 /**
