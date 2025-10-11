bc44da5e6196b48f7d36f17c37242cecae155963
jovkb <jovani.badinga7@gmail.com>
2025-10-11 11:14:47 -0400

    Server Load (10 Marks) Completed
    This framework is suprisingly very simple to use.

--- AssignmentTwo/server.js
+++ AssignmentTwo/server.js
@@ -1,16 +1,45 @@
-//Base code server for Assignment 2, COMP 2406, Fall 2025
-//author: andrew runka
-//date: fall 2025
+// run server via 'node server.js'
+const restaurantObjs = [];
 
-//Simple static file server to serve files from ./client directory
-//e.g. http://localhost:2406/order.html serves ./client/order.html
-
-//Run using > node server.js
-//TODO: implement API routes to serve restaurant data
-//      implement dynamic stats page rendering using pug
-//      implement order submission handling
+const port = 8000;
+const host = "localhost";
+const restaurantsDirPath = "./restaurants";
 
 const http = require('http');
 const fs = require('fs');
 const urlLib = require('url');
+const path = require('path');
 
+readRestaurantInfo();
+
+// block executed on every incoming request
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    res.end("You are connected to the server....");
+})
+// block executed on server startup
+server.listen(port, host, () => {
+    console.log(`Server running at https://${host}:${port}`);
+    console.log(restaurantObjs);
+})
+
+/**
+ * Reads all the .json files in the restaurants dir, converts them into JSON objects and pushes them
+ * to the `restaurantObjs` list
+ *
+ * - Note: This function runs entirely synchronously
+ */
+function readRestaurantInfo() {
+    try {
+        const files = fs.readdirSync(restaurantsDirPath);
+        files.forEach(file => {
+            if (path.extname(file) !== '.json') return;
+            const data = fs.readFileSync(`${restaurantsDirPath}/${file}`, {encoding: "utf-8"});
+            restaurantObjs.push(JSON.parse(data));
+        });
+    } catch (err) {
+        console.log(`Error reading restaurant files: ${err}`);
+    }
+}
